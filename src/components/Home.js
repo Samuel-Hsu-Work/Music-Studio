56fa6e0c7bf2130b186e8af00cf5a50eb100e29c
sky19930112 <sky19930112@gmail.com>
2023-06-28 01:59:38 -0500

    work on Navbar

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#00df9a] font-bold p-2 sm:text-6xl'>
+          Resources for your journey in music world
+        </p>
+        <h1 className='md:text-4xl sm:text-6x1 text-4xl font-bold md:py-6'>
+          Whithout Music, your life will Bb.
+        </h1>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'></p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import { Link, animateScroll as scroll, } from 'react-scroll'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -10,24 +11,26 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>三秒 Music Page</h1>
       <ul className='hidden md:flex'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Company</li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
+        <li className='p-4'><Link to="Home" smooth={true} duration={500}>Home</Link></li>
+        <li className='p-4'><Link to="Music Theory" smooth={true} offset={-200} duration={500}>Music Theory</Link></li>
+        <li className='p-4'><Link to="Terms" smooth={true} offset={-50} duration={500}>Terms</Link></li>
+        <li className='p-4'><Link to="Tools" smooth={true} offset={-100} duration={500}>Tools</Link></li>
+        <li className='p-4'><Link to="practice" smooth={true} offset={-50} duration={500}>practice</Link></li>
+        <li className='p-4'><Link to="contact" smooth={true} offset={-50} duration={500}>contact</Link></li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>三秒 Music Page</h1>
+          <li className='p-4 border-b border-gray-600'><Link to="Home" smooth={true} duration={500}>Home</Link></li>
+          <li className='p-4 border-b border-gray-600'><Link to="Music Theory" smooth={true} offset={-200} duration={500}>Music Theory</Link></li>
+          <li className='p-4 border-b border-gray-600'><Link to="Terms" smooth={true} offset={-50} duration={500}>Terms</Link></li>
+          <li className='p-4 border-b border-gray-600'><Link to="Tools" smooth={true} offset={-50} duration={500}>Tools</Link></li>
+          <li className='p-4 border-b border-gray-600'><Link to="practice" smooth={true} offset={-50} duration={500}>practice</Link></li>
+          <li className='p-4 border-b border-gray-600'><Link to="contact" smooth={true} offset={-50} duration={500}>contact</Link></li>
       </ul>
     </div>
   );
